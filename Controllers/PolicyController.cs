cd5f8d010e94a4c67b13d81cb38fa2540cd1f48b
codinci <dkibau@gmail.com>
2025-01-15 06:00:48 +0300

    Added Controller

---
+++ Controllers/PolicyController.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using InsurancePolicyApi.Models;
+
+namespace InsurancePolicyApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PolicyController : ControllerBase
+    {
+        private readonly PolicyContext _context;
+
+        public PolicyController(PolicyContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/Policy
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Policy>>> GetPolicies()
+        {
+            return await _context.Policies.ToListAsync();
+        }
+
+        // GET: api/Policy/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Policy>> GetPolicy(int id)
+        {
+            var policy = await _context.Policies.FindAsync(id);
+
+            if (policy == null)
+            {
+                return NotFound();
+            }
+
+            return policy;
+        }
+
+        // PUT: api/Policy/5
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutPolicy(int id, Policy policy)
+        {
+            if (id != policy.PolicyId)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(policy).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!PolicyExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/Policy
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPost]
+        public async Task<ActionResult<Policy>> PostPolicy(Policy policy)
+        {
+            _context.Policies.Add(policy);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("GetPolicy", new { id = policy.PolicyId }, policy);
+        }
+
+        // DELETE: api/Policy/5
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeletePolicy(int id)
+        {
+            var policy = await _context.Policies.FindAsync(id);
+            if (policy == null)
+            {
+                return NotFound();
+            }
+
+            _context.Policies.Remove(policy);
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+
+        private bool PolicyExists(int id)
+        {
+            return _context.Policies.Any(e => e.PolicyId == id);
+        }
+    }
+}
